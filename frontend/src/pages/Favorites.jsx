fcfa6a3c5f2a21166bb8bcf47c1830a7992c7f6e
Tharushirdtk <kavindirdtk@gmail.com>
2025-05-23 06:52:32 +0530

    make favorites page

---
+++ frontend/src/pages/Favorites.jsx
@@ -0,0 +1,8 @@
+function Favorites(){
+    return <div className="favourites-empty">
+        <h2>No favourite movies yet</h2>
+        <p>Start adding movies to your favourites and they will appear here!</p>
+    </div>
+}
+
+export default Favorites
