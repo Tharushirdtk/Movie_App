66ae04b18b54485d78e8fe17d2514cc78a4bb07e
Tharushirdtk <kavindirdtk@gmail.com>
2025-05-23 06:54:20 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,27 @@
 
 import './App.css'
+import Favorites from './pages/Favorites';
+import Home from './pages/Home';
+import {Routes, Route} from "react-router-dom";
+import NavBar from './components/NavBar';
 
 function App() {
   
   return (
-    <>
-    
-    
-    </>
+    <div>
+      <NavBar/>
+      <main className='main-content'>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/Favorites' element={<Favorites/>}></Route>
+      </Routes>
+    </main>
    
+    </div>
+    
   );
 }
 
 
 
-export default App
+export default App;
